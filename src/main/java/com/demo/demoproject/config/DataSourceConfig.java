d6043b6b16412d15d69240c0f82924ab8859f086
Nikita Mykhailov <nmykhailov@ebay.com>
2022-01-24 20:48:36 +0100

    add DataSourceConfig

---
+++ src/main/java/com/demo/demoproject/config/DataSourceConfig.java
@@ -0,0 +1,30 @@
+package com.demo.demoproject.config;
+
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfig {
+
+  @Value("${JDBC_DATABASE_URL}")
+  private String url;
+
+  @Value("${JDBC_DATABASE_PASSWORD}")
+  private String password;
+
+  @Value("${JDBC_DATABASE_USERNAME}")
+  private String username;
+
+  @Bean
+  public DataSource dataSource() {
+    DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+    dataSourceBuilder.driverClassName("org.postgresql.Driver");
+    dataSourceBuilder.url(url);
+    dataSourceBuilder.username(username);
+    dataSourceBuilder.password(password);
+    return dataSourceBuilder.build();
+  }
+}
