5c5be3400d75e3c6149f2f98f2e77ea93e92dd12
Nikita Mykhailov <nmykhailov@ebay.com>
2022-01-27 16:30:03 +0100

    remove liquiase

--- src/main/java/com/demo/demoproject/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.demo.demoproject.repository;
-
-import com.demo.demoproject.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
