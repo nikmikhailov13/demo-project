41a378208ecf3bd9b8a3946f605825782baa273d
Nikita Mykhailov <nmykhailov@ebay.com>
2022-01-27 20:56:53 +0100

    add spring data

--- src/main/java/com/demo/demoproject/controller/PingController.java
+++ src/main/java/com/demo/demoproject/controller/PingController.java
@@ -1,13 +1,22 @@
 package com.demo.demoproject.controller;
 
+import com.demo.demoproject.entity.User;
+import com.demo.demoproject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingController {
 
+  @Autowired
+  private UserRepository userRepository;
+
   @GetMapping("/ping")
   public String ping() {
+    User user = new User();
+    user.setName("Nikita");
+    userRepository.save(user);
 
     return "pong";
   }
---
+++ src/main/java/com/demo/demoproject/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.demo.demoproject.repository;
+
+import com.demo.demoproject.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
